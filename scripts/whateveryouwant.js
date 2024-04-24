fc730f9d61102eb40520fb35e7e7c7b32e7dc678
maddiedemos27 <144718708+maddiedemos27@users.noreply.github.com>
2024-04-24 18:39:31 +0000

    EA,GJB

--- scripts/whateveryouwant.js
+++ scripts/whateveryouwant.js
@@ -1,11 +1,18 @@
-//task #1//
-console.log("Task 1");
+console.log("Task #1");
 for (let i = 0; i <=10; i++) {
     console.log(i*i);
 }
 
-//task #2//
-for (let i = 5; i > 0; i--) {
-    println(i);
+concole.log("Task #2");
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+    console.log("Blast off!");
 }
 
+console.log("Task #3");
+for (let i = 1; i <= 50; i++) {
+    console.log(i / 2);
+}
+
+console.log("Task #4");
+for (let)

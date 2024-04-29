4dd3a36d4068f91db9abd1661a08e52ab0847f28
maddiedemos27 <144718708+maddiedemos27@users.noreply.github.com>
2024-04-29 17:11:16 +0000

    wfjpgoiqg

--- scripts/whateveryouwant.js
+++ scripts/whateveryouwant.js
@@ -5,8 +5,8 @@ for (let i = 0; i <= 10; i++) {
 console.log("Task #2");
 for (let i = 5; i >= 1; i--) {
     console.log(i);
-    console.log("Blast off!");
 }
+console.log("Blast off!");
 
 console.log("Task #3");
 for (let i = 0; i <= 50; i += 2) {
@@ -14,11 +14,15 @@ for (let i = 0; i <= 50; i += 2) {
 }
 
 console.log("Task #4");
-let sumNumbers = i + i;
+let totalNumbers = 0;
+let sumNumbers = 0;
 for (let i = 1; i <= 100; i++) {
-    console.log(i + " addition  = " + sumNumbers;
+    sumNumbers = sumNumbers + i; 
+    console.log("" + sumNumbers)
 }
 
+
+// "" + sumNumbers //
 // console.log("Task #5");
 // for (let i = 1; 1 <= 10; i++) {
 //     console.log(i*(i-1));

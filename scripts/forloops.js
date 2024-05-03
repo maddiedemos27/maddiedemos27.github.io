09976c5619e104888f662ca6c75967564f8c12e9
maddiedemos27 <144718708+maddiedemos27@users.noreply.github.com>
2024-05-03 19:40:23 +0000

    ascpcksdp

---
+++ scripts/forloops.js
@@ -0,0 +1,28 @@
+console.log("Task #1");
+for (let i = 0; i <= 10; i++) {
+    console.log(i*i);
+}
+console.log("Task #2");
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+console.log("Blast off!");
+
+console.log("Task #3");
+for (let i = 0; i <= 50; i += 2) {
+    console.log(i);
+}
+
+console.log("Task #4");
+let sumNumbers = 0;
+for (let i = 1; i <= 100; i++) {
+    sumNumbers = sumNumbers + i; 
+}
+console.log(sumNumbers);
+
+console.log("Task #5");
+let numbersFactorial = 1;
+for (let i = 1; i <= 10; i++) {
+    numbersFactorial= numbersFactorial * i
+    console.log(numbersFactorial);
+}
